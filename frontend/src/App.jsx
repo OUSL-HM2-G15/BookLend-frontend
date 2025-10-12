4113ed3cf1ffbe1d6ef3ad5c42e4488b6ccb71c8
DESKTOP-MJ6C7L1\YVK <s22010502@ousl.lk>
2025-10-12 11:46:40 +0530

    Modified the BookLend name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6"> BookSphere</h1>
+      <h1 className="text-3xl font-bold mb-6"> BookLend</h1>
 
       <button
         onClick={() => setShowLogin(true)}
