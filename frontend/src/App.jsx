f1d16b44cd63d12208e82424ad847a9cb1df5ae9
zainab fathima <zainab9j3@gmail.com>
2025-10-22 13:00:48 +0530

    feat: Add project structure (pages, components, utils, services, assets)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-
+import 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline text-blue-600">
-      Your frontend is successfully work with Taiwind CSS!
+      Your frontend is successfully working with Taiwind CSS!
     </h1>
   );
 }
