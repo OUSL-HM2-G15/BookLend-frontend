b8615e500cc305c230ea2e111663a9d0ff523ecf
zainab fathima <zainab9j3@gmail.com>
2026-01-10 01:38:33 +0530

    feat: implement book lended section

--- frontend/src/pages/RequestsPosted.jsx
+++
@@ -1,8 +0,0 @@
-export default function RequestsPosted() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Requests Posted</h1>
-      <p>This is where the list of requests I have posted will be displayed here, those are the requests for books that are currently not available.</p>
-    </div>
-  );
-}
